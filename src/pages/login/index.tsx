155727af91b5b27518ad5edcef51e830696f66b4
rayhomie <1572801584@qq.com>
2021-03-23 19:55:27 +0800

    登录注册页面

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -8,7 +8,12 @@ import {
   StatusBar,
   TouchableOpacity,
 } from 'react-native';
-import { Button, Modal } from '@ant-design/react-native';
+import {
+  Button,
+  Modal,
+  InputItem,
+  SegmentedControl,
+} from '@ant-design/react-native';
 import { ConnectProps, ConnectState } from '@/models/connect';
 import { connect } from '@/utils/connect';
 
@@ -18,20 +23,97 @@ interface IProps extends ConnectState, ConnectProps {
   onClose: () => void;
 }
 
-interface IState {}
+interface IState {
+  showLogin: boolean;
+  gender: number;
+}
 @connect(({ app, loading }: IProps) => ({
   app,
   dataLoading: loading.effects['app/login'],
 }))
 export default class Login extends Component<IProps, IState> {
+  state: IState = {
+    showLogin: true,
+    gender: 1,
+  };
+
+  handleShowLogin = (value: string) => {
+    this.setState({ showLogin: value === '登录' ? true : false });
+  };
+
+  handleSex = (value: string) => {
+    this.setState({ gender: value === '男' ? 1 : 0 });
+  };
+
   render() {
     const { visible, onClose } = this.props;
     return (
       <Modal popup visible={visible}>
         <View style={styles.container}>
-          <Button>登录</Button>
-          <Button>注册</Button>
-          <Button onPress={onClose}>关闭</Button>
+          <SegmentedControl
+            tintColor="#ffb8c4"
+            values={['登录', '注册']}
+            style={styles.tabs}
+            onValueChange={this.handleShowLogin}
+          />
+          {this.state.showLogin ? (
+            <View style={styles.login}>
+              <InputItem
+                clear
+                placeholder="请输入用户名"
+                name="username"
+                maxLength={15}
+              >
+                用户名：
+              </InputItem>
+              <InputItem
+                type="password"
+                clear
+                placeholder="请输入密码"
+                name="password"
+              >
+                密码：
+              </InputItem>
+              <Button>登录</Button>
+            </View>
+          ) : (
+            <View style={styles.register}>
+              <InputItem
+                clear
+                placeholder="请输入用户名"
+                name="username"
+                maxLength={15}
+              >
+                用户名：
+              </InputItem>
+              <InputItem
+                type="password"
+                clear
+                placeholder="请输入密码"
+                name="password"
+              >
+                密码：
+              </InputItem>
+              <View style={styles.sex}>
+                <Text style={styles.sexText}>性别：</Text>
+                <SegmentedControl
+                  values={['男', '女']}
+                  style={styles.sexControl}
+                  onValueChange={this.handleSex}
+                />
+              </View>
+              <InputItem
+                type="phone"
+                clear
+                placeholder="请输入手机号"
+                name="password"
+              >
+                手机号
+              </InputItem>
+              <Button>注册</Button>
+            </View>
+          )}
+          <Button onPress={onClose}>收起</Button>
         </View>
       </Modal>
     );
@@ -42,5 +124,20 @@ const screenWidth = Dimensions.get('window').width;
 const screenHeight = Dimensions.get('window').height;
 
 const styles = StyleSheet.create({
-  container: { width: screenWidth, height: screenHeight, marginTop: 200 },
+  container: { width: screenWidth, height: screenHeight, marginTop: 100 },
+  tabs: { width: screenWidth },
+  login: {},
+  register: {},
+  sex: {
+    width: screenWidth,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    borderBottomColor: '#eee',
+    borderBottomWidth: 1,
+    paddingTop: 10,
+    paddingBottom: 10,
+  },
+  sexText: { fontSize: 17, marginLeft: 15 },
+  sexControl: { width: 200, height: 30, marginLeft: 30 },
 });

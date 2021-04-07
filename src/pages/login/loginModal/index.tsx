7219652239404cb491e5ca8b358c134e4dbf3775
rayhomie <1572801584@qq.com>
2021-04-07 13:32:53 +0800

    login和registermodal改函数组件

--- src/pages/login/loginModal/index.tsx
+++ src/pages/login/loginModal/index.tsx
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Dimensions, View, StyleSheet } from 'react-native';
 import { Button, Modal, Toast } from '@ant-design/react-native';
 import { ConnectProps, ConnectState, Dispatch } from '@/models/connect';
-import { connect } from '@/utils/connect';
+import { connect } from 'react-redux';
 const { GiftedForm, GiftedFormManager } = require('react-native-gifted-form');
 
 interface IProps extends ConnectState, ConnectProps {
@@ -15,10 +15,6 @@ type LoginFormType = {
   password: string;
 };
 
-interface IState {
-  loginForm: LoginFormType;
-}
-
 const usernameRegex = /^[a-zA-Z0-9_]{3,16}$/;
 const passwordNoSpaceRegex = /^[^\s]*$/;
 // 最少6位，包括至少1个小写字母，1个数字
@@ -30,125 +26,113 @@ const getValue = (key: string[]) =>
     return pre;
   }, {});
 
-@connect(({ app, user, loading }: any) => ({
-  app,
-  user,
-  dataLoading: loading.effects['app/login'],
-}))
-export default class LoginModal extends Component<IProps, IState> {
-  state: IState = {
-    loginForm: {
-      username: '',
-      password: '',
-    },
+const LoginModal: React.FC<IProps> = props => {
+  const { visible, onClose, dispatch } = props;
+  const [loginForm, setLoginForm] = useState<LoginFormType>({
+    username: '',
+    password: '',
+  });
+
+  const handleLoginValueChange = (values: LoginFormType) => {
+    setLoginForm(values);
   };
 
-  handleLoginValueChange = (values: LoginFormType) => {
-    this.setState({ loginForm: values });
-  };
-
-  render() {
-    const { visible, onClose, dispatch } = this.props;
-    return (
-      <Modal animationType="slide" visible={visible}>
-        <View style={styles.container}>
-          <GiftedForm
-            formName="loginForm"
-            onValueChange={this.handleLoginValueChange}
-            validators={{
-              username: {
-                title: '用户名',
-                validate: [
-                  {
-                    validator: 'isLength',
-                    arguments: [3, 16],
-                    message: '{TITLE}长度为{ARGS[0]}到{ARGS[1]}个字符',
+  return (
+    <Modal animationType="slide" visible={visible}>
+      <View style={styles.container}>
+        <GiftedForm
+          formName="loginForm"
+          onValueChange={handleLoginValueChange}
+          validators={{
+            username: {
+              title: '用户名',
+              validate: [
+                {
+                  validator: 'isLength',
+                  arguments: [3, 16],
+                  message: '{TITLE}长度为{ARGS[0]}到{ARGS[1]}个字符',
+                },
+                {
+                  validator: 'matches',
+                  arguments: usernameRegex,
+                  message:
+                    '{TITLE}是由a～z或A~Z的英文字母、0～9的数字或下划线组成',
+                },
+              ],
+            },
+            password: {
+              title: '密码',
+              validate: [
+                {
+                  validator: 'isLength',
+                  arguments: [6, 16],
+                  message: '{TITLE}长度为{ARGS[0]}到{ARGS[1]}个字符',
+                },
+                {
+                  validator: 'matches',
+                  arguments: passwordRegex,
+                  message: '{TITLE}至少包括1个小写字母，1个数字',
+                },
+                {
+                  validator: 'matches',
+                  arguments: passwordNoSpaceRegex,
+                  message: '{TITLE}中不能使用空格',
+                },
+              ],
+            },
+          }}
+        >
+          <GiftedForm.TextInputWidget
+            name="username"
+            title="用户名"
+            placeholder="请输入用户名"
+            clearButtonMode="while-editing"
+            maxLength={16}
+            value={loginForm.username}
+          />
+          <GiftedForm.TextInputWidget
+            name="password"
+            title="密码"
+            placeholder="请输入密码"
+            clearButtonMode="while-editing"
+            maxLength={16}
+            value={loginForm.password}
+            secureTextEntry={true}
+          />
+          <Button
+            onPress={async () => {
+              const { username, password } = getValue(['username', 'password']);
+              await (dispatch as Dispatch)({
+                type: 'app/login',
+                payload: {
+                  username,
+                  password,
+                  success: () => {
+                    Toast.success('登录成功', 1.5, () => onClose());
+                    (dispatch as Dispatch)({
+                      type: 'user/getUserInfo',
+                      payload: {
+                        success: () => {},
+                        fail: () => {
+                          Toast.fail('获取用户信息失败');
+                        },
+                      },
+                    });
                   },
-                  {
-                    validator: 'matches',
-                    arguments: usernameRegex,
-                    message:
-                      '{TITLE}是由a～z或A~Z的英文字母、0～9的数字或下划线组成',
-                  },
-                ],
-              },
-              password: {
-                title: '密码',
-                validate: [
-                  {
-                    validator: 'isLength',
-                    arguments: [6, 16],
-                    message: '{TITLE}长度为{ARGS[0]}到{ARGS[1]}个字符',
-                  },
-                  {
-                    validator: 'matches',
-                    arguments: passwordRegex,
-                    message: '{TITLE}至少包括1个小写字母，1个数字',
-                  },
-                  {
-                    validator: 'matches',
-                    arguments: passwordNoSpaceRegex,
-                    message: '{TITLE}中不能使用空格',
-                  },
-                ],
-              },
+                  fail: () =>
+                    Toast.fail('登录失败，请检查账号或密码是否正确', 1.5),
+                },
+              });
             }}
           >
-            <GiftedForm.TextInputWidget
-              name="username"
-              title="用户名"
-              placeholder="请输入用户名"
-              clearButtonMode="while-editing"
-              maxLength={16}
-              value={this.state.loginForm.username}
-            />
-            <GiftedForm.TextInputWidget
-              name="password"
-              title="密码"
-              placeholder="请输入密码"
-              clearButtonMode="while-editing"
-              maxLength={16}
-              value={this.state.loginForm.password}
-              secureTextEntry={true}
-            />
-            <Button
-              onPress={async () => {
-                const { username, password } = getValue([
-                  'username',
-                  'password',
-                ]);
-                await (dispatch as Dispatch)({
-                  type: 'app/login',
-                  payload: {
-                    username,
-                    password,
-                    success: () => {
-                      Toast.success('登录成功', 1.5, () => onClose());
-                      (dispatch as Dispatch)({
-                        type: 'user/getUserInfo',
-                        payload: {
-                          success: () => {},
-                          fail: () => {
-                            Toast.fail('获取用户信息失败');
-                          },
-                        },
-                      });
-                    },
-                    fail: () =>
-                      Toast.fail('登录失败，请检查账号或密码是否正确', 1.5),
-                  },
-                });
-              }}
-            >
-              登录
-            </Button>
-            <Button onPress={onClose}>收起</Button>
-          </GiftedForm>
-        </View>
-      </Modal>
-    );
-  }
-}
+            登录
+          </Button>
+          <Button onPress={onClose}>收起</Button>
+        </GiftedForm>
+      </View>
+    </Modal>
+  );
+};
 
 const screenWidth = Dimensions.get('window').width;
 const screenHeight = Dimensions.get('window').height;
@@ -156,3 +140,11 @@ const screenHeight = Dimensions.get('window').height;
 const styles = StyleSheet.create({
   container: { width: screenWidth, height: screenHeight, marginTop: 100 },
 });
+
+export default connect(({ app, user, mine, record, loading }: IProps) => ({
+  app,
+  user,
+  record,
+  mine,
+  dataLoading: loading?.effects['app/login'],
+}))(LoginModal);
--- src/pages/login/registerModal/index.tsx
+++ src/pages/login/registerModal/index.tsx
@@ -1,4 +1,5 @@
-import React, { Component } from 'react';
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useState, useEffect } from 'react';
 import { Dimensions, View, Text, StyleSheet } from 'react-native';
 import {
   Button,
@@ -7,7 +8,7 @@ import {
   Toast,
 } from '@ant-design/react-native';
 import { ConnectProps, ConnectState, Dispatch } from '@/models/connect';
-import { connect } from '@/utils/connect';
+import { connect } from 'react-redux';
 const { GiftedForm, GiftedFormManager } = require('react-native-gifted-form');
 
 interface IProps extends ConnectState, ConnectProps {
@@ -23,11 +24,6 @@ type RegisterFormType = {
   email: string;
 };
 
-interface IState {
-  gender: number;
-  registerForm: RegisterFormType;
-}
-
 const usernameRegex = /^[a-zA-Z0-9_]{3,16}$/;
 const passwordNoSpaceRegex = /^[^\s]*$/;
 // 最少6位，包括至少1个小写字母，1个数字
@@ -41,224 +37,215 @@ const getValue = (key: string[]) =>
     return pre;
   }, {});
 
-@connect(({ app, loading }: any) => ({
-  app,
-  dataLoading: loading.effects['app/login'],
-}))
-export default class RegisterModal extends Component<IProps, IState> {
-  state: IState = {
-    gender: 1,
-    registerForm: {
-      username: '',
-      confirmPassword: '',
-      password: '',
-      mobile_number: '',
-      email: '',
-    },
+const RegisterModal: React.FC<IProps> = props => {
+  const { visible, onClose, dispatch } = props;
+  const [gender, setGender] = useState<number>(1);
+  const [registerForm, setRegisterForm] = useState<RegisterFormType>({
+    username: '',
+    confirmPassword: '',
+    password: '',
+    mobile_number: '',
+    email: '',
+  });
+
+  const handleSex = (value: string) => {
+    setGender(value === '男' ? 1 : 0);
   };
 
-  handleSex = (value: string) => {
-    this.setState({ gender: value === '男' ? 1 : 0 });
+  const handleRegisterValueChange = (values: RegisterFormType) => {
+    setRegisterForm(values);
   };
 
-  handleRegisterValueChange = (values: RegisterFormType) => {
-    this.setState({ registerForm: values });
-  };
-
-  componentDidMount() {
-    const { dispatch } = this.props;
+  useEffect(() => {
     (dispatch as Dispatch)({ type: 'save', payload: { isRegister: false } });
-  }
+  }, []);
 
-  render() {
-    const { visible, onClose, dispatch } = this.props;
-    return (
-      <Modal animationType="slide" visible={visible}>
-        <View style={styles.container}>
-          <GiftedForm
-            formName="registerForm"
-            onValueChange={this.handleRegisterValueChange}
-            clearOnClose
-            validators={{
-              username: {
-                title: '用户名',
-                validate: [
-                  {
-                    validator: 'isLength',
-                    arguments: [3, 16],
-                    message: '{TITLE}长度为{ARGS[0]}到{ARGS[1]}个字符',
+  return (
+    <Modal animationType="slide" visible={visible}>
+      <View style={styles.container}>
+        <GiftedForm
+          formName="registerForm"
+          onValueChange={handleRegisterValueChange}
+          clearOnClose
+          validators={{
+            username: {
+              title: '用户名',
+              validate: [
+                {
+                  validator: 'isLength',
+                  arguments: [3, 16],
+                  message: '{TITLE}长度为{ARGS[0]}到{ARGS[1]}个字符',
+                },
+                {
+                  validator: 'matches',
+                  arguments: usernameRegex,
+                  message:
+                    '{TITLE}是由a～z或A~Z的英文字母、0～9的数字或下划线组成',
+                },
+              ],
+            },
+            password: {
+              title: '密码',
+              validate: [
+                {
+                  validator: 'isLength',
+                  arguments: [6, 16],
+                  message: '{TITLE}长度为{ARGS[0]}到{ARGS[1]}个字符',
+                },
+                {
+                  validator: 'matches',
+                  arguments: passwordRegex,
+                  message: '{TITLE}至少包括1个小写字母，1个数字',
+                },
+                {
+                  validator: 'matches',
+                  arguments: passwordNoSpaceRegex,
+                  message: '{TITLE}中不能使用空格',
+                },
+              ],
+            },
+            confirmPassword: {
+              title: '确认密码',
+              validate: [
+                {
+                  validator: (value: string) => {
+                    if (value === registerForm.password) {
+                      return true;
+                    }
+                    return false;
                   },
-                  {
-                    validator: 'matches',
-                    arguments: usernameRegex,
-                    message:
-                      '{TITLE}是由a～z或A~Z的英文字母、0～9的数字或下划线组成',
-                  },
-                ],
-              },
-              password: {
-                title: '密码',
-                validate: [
-                  {
-                    validator: 'isLength',
-                    arguments: [6, 16],
-                    message: '{TITLE}长度为{ARGS[0]}到{ARGS[1]}个字符',
-                  },
-                  {
-                    validator: 'matches',
-                    arguments: passwordRegex,
-                    message: '{TITLE}至少包括1个小写字母，1个数字',
-                  },
-                  {
-                    validator: 'matches',
-                    arguments: passwordNoSpaceRegex,
-                    message: '{TITLE}中不能使用空格',
-                  },
-                ],
-              },
-              confirmPassword: {
-                title: '确认密码',
-                validate: [
-                  {
-                    validator: (value: string) => {
-                      if (value === this.state.registerForm.password) {
-                        return true;
-                      }
-                      return false;
-                    },
-                    message: '请保证两次密码一致',
-                  },
-                ],
-              },
-              email: {
-                title: '邮箱',
-                validate: [
-                  {
-                    validator: 'matches',
-                    arguments: emailRegex,
-                    message: '请按照正确{TITLE}格式输入',
-                  },
-                ],
-              },
-              mobile_number: {
-                title: '手机号',
-                validate: [
-                  {
-                    validator: 'matches',
-                    arguments: phoneRegex,
-                    message: '请正确{TITLE}格式输入',
-                  },
-                ],
-              },
+                  message: '请保证两次密码一致',
+                },
+              ],
+            },
+            email: {
+              title: '邮箱',
+              validate: [
+                {
+                  validator: 'matches',
+                  arguments: emailRegex,
+                  message: '请按照正确{TITLE}格式输入',
+                },
+              ],
+            },
+            mobile_number: {
+              title: '手机号',
+              validate: [
+                {
+                  validator: 'matches',
+                  arguments: phoneRegex,
+                  message: '请正确{TITLE}格式输入',
+                },
+              ],
+            },
+          }}
+        >
+          <GiftedForm.TextInputWidget
+            name="username"
+            title="用户名"
+            maxLength={16}
+            placeholder="请填写用户名"
+            clearButtonMode="while-editing"
+            value={registerForm.username}
+          />
+          <GiftedForm.TextInputWidget
+            name="password"
+            title="密码"
+            maxLength={16}
+            placeholder="请填写密码"
+            clearButtonMode="while-editing"
+            value={registerForm.password}
+            secureTextEntry={true}
+          />
+          <GiftedForm.TextInputWidget
+            name="confirmPassword"
+            title="确认密码"
+            maxLength={16}
+            placeholder="请再次确认密码"
+            clearButtonMode="while-editing"
+            value={registerForm.confirmPassword}
+            secureTextEntry={true}
+          />
+          <View style={styles.sex}>
+            <Text style={styles.sexText}>性别</Text>
+            <SegmentedControl
+              values={['男', '女']}
+              style={styles.sexControl}
+              onValueChange={handleSex}
+            />
+          </View>
+          <GiftedForm.TextInputWidget
+            name="email"
+            title="邮箱"
+            keyboardType="email-address"
+            placeholder="example@nomads.ly"
+            clearButtonMode="while-editing"
+            value={registerForm.email}
+          />
+          <GiftedForm.TextInputWidget
+            name="mobile_number"
+            title="手机号"
+            maxLength={11}
+            keyboardType="phone-pad"
+            placeholder="请输入11位手机号嘛"
+            clearButtonMode="while-editing"
+            value={registerForm.mobile_number}
+          />
+          <Button
+            onPress={async () => {
+              const data = getValue([
+                'username',
+                'password',
+                'confirmPassword',
+                'mobile_number',
+                'email',
+              ]);
+              data.gender = gender;
+              if (!usernameRegex.test(data.username)) {
+                Toast.fail(
+                  '用户名是由3至16位，a～z或A~Z的英文字母、0～9的数字或下划线组成',
+                  1.5
+                );
+                return;
+              }
+              if (!passwordRegex.test(data.password)) {
+                Toast.fail(
+                  '密码由至少包括1个小写字母，1个数字组成，且设置至少6位密码',
+                  1.5
+                );
+                return;
+              }
+              if (data.confirmPassword !== data.password) {
+                Toast.fail('请保持两次密码一致', 1.5);
+                return;
+              }
+              if (!emailRegex.test(data.email)) {
+                Toast.fail('请按照正确邮箱格式输入', 1.5);
+                return;
+              }
+              if (!phoneRegex.test(data.mobile_number)) {
+                Toast.fail('请按照正确手机号格式输入', 1.5);
+                return;
+              }
+              await (dispatch as Dispatch)({
+                type: 'app/register',
+                payload: {
+                  ...data,
+                  success: () =>
+                    Toast.success('注册成功', 1.5, () => onClose()),
+                  fail: () => Toast.fail('用户名已存在，注册失败', 1.5),
+                },
+              });
             }}
           >
-            <GiftedForm.TextInputWidget
-              name="username"
-              title="用户名"
-              maxLength={16}
-              placeholder="请填写用户名"
-              clearButtonMode="while-editing"
-              value={this.state.registerForm.username}
-            />
-            <GiftedForm.TextInputWidget
-              name="password"
-              title="密码"
-              maxLength={16}
-              placeholder="请填写密码"
-              clearButtonMode="while-editing"
-              value={this.state.registerForm.password}
-              secureTextEntry={true}
-            />
-            <GiftedForm.TextInputWidget
-              name="confirmPassword"
-              title="确认密码"
-              maxLength={16}
-              placeholder="请再次确认密码"
-              clearButtonMode="while-editing"
-              value={this.state.registerForm.confirmPassword}
-              secureTextEntry={true}
-            />
-            <View style={styles.sex}>
-              <Text style={styles.sexText}>性别</Text>
-              <SegmentedControl
-                values={['男', '女']}
-                style={styles.sexControl}
-                onValueChange={this.handleSex}
-              />
-            </View>
-            <GiftedForm.TextInputWidget
-              name="email"
-              title="邮箱"
-              keyboardType="email-address"
-              placeholder="example@nomads.ly"
-              clearButtonMode="while-editing"
-              value={this.state.registerForm.email}
-            />
-            <GiftedForm.TextInputWidget
-              name="mobile_number"
-              title="手机号"
-              maxLength={11}
-              keyboardType="phone-pad"
-              placeholder="请输入11位手机号嘛"
-              clearButtonMode="while-editing"
-              value={this.state.registerForm.mobile_number}
-            />
-            <Button
-              onPress={async () => {
-                const data = getValue([
-                  'username',
-                  'password',
-                  'confirmPassword',
-                  'mobile_number',
-                  'email',
-                ]);
-                data.gender = this.state.gender;
-                if (!usernameRegex.test(data.username)) {
-                  Toast.fail(
-                    '用户名是由3至16位，a～z或A~Z的英文字母、0～9的数字或下划线组成',
-                    1.5
-                  );
-                  return;
-                }
-                if (!passwordRegex.test(data.password)) {
-                  Toast.fail(
-                    '密码由至少包括1个小写字母，1个数字组成，且设置至少6位密码',
-                    1.5
-                  );
-                  return;
-                }
-                if (data.confirmPassword !== data.password) {
-                  Toast.fail('请保持两次密码一致', 1.5);
-                  return;
-                }
-                if (!emailRegex.test(data.email)) {
-                  Toast.fail('请按照正确邮箱格式输入', 1.5);
-                  return;
-                }
-                if (!phoneRegex.test(data.mobile_number)) {
-                  Toast.fail('请按照正确手机号格式输入', 1.5);
-                  return;
-                }
-                await (dispatch as Dispatch)({
-                  type: 'app/register',
-                  payload: {
-                    ...data,
-                    success: () =>
-                      Toast.success('注册成功', 1.5, () => onClose()),
-                    fail: () => Toast.fail('用户名已存在，注册失败', 1.5),
-                  },
-                });
-              }}
-            >
-              注册
-            </Button>
-            <Button onPress={onClose}>收起</Button>
-          </GiftedForm>
-        </View>
-      </Modal>
-    );
-  }
-}
+            注册
+          </Button>
+          <Button onPress={onClose}>收起</Button>
+        </GiftedForm>
+      </View>
+    </Modal>
+  );
+};
 
 const screenWidth = Dimensions.get('window').width;
 const screenHeight = Dimensions.get('window').height;
@@ -279,3 +266,11 @@ const styles = StyleSheet.create({
   sexText: { fontSize: 15, marginLeft: 10 },
   sexControl: { width: 200, height: 30, marginLeft: 55 },
 });
+
+export default connect(({ app, user, mine, record, loading }: IProps) => ({
+  app,
+  user,
+  record,
+  mine,
+  dataLoading: loading?.effects['app/login'],
+}))(RegisterModal);
